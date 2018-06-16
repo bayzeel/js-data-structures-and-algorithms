fd3056dfdd516f5f5ef9bb1a672b2b56e681a1e3
Vasyl Sklepkovych <vsklepkovich@gmail.com>
2018-06-16 14:23:02 +0300

    Adding value to the first and last position, removing value from the first position

--- data-structures/arrays.js
+++ data-structures/arrays.js
@@ -12,5 +12,37 @@ function fibonacciInLoop ( size ) {
 
   return arr;
 }
-
 console.log( 'fibonacciInLoop', fibonacciInLoop( 20 ) );
+
+
+function addValueToTheLastPosOfTheArray ( arr, value ) {
+  arr[arr.length] = value;
+
+  return arr;
+}
+console.log( 'addValueToTheLastPosOfTheArray', addValueToTheLastPosOfTheArray( [1, 2, 3, 4], 5 ) );
+
+
+function addValueToTheFirstPosOfTheArray ( arr, value ) {
+  var arrSize = arr.length;
+
+  for ( var i = arrSize; i >= 0; i-- ) {
+    arr[i] = arr[i-1];
+  }
+  arr[0] = value;
+
+  return arr;
+}
+console.log( 'addValueToTheFirstPosOfTheArray', addValueToTheFirstPosOfTheArray( [2, 3, 4, 5], 1 ) );
+
+function removeValueFromTheFirstPosOfTheArray ( arr ) {
+  var arrSize = arr.length;
+
+  for ( var i = 0; i < arrSize; i++ ) {
+    arr[i] = arr[i+1];
+  }
+  arr.length = arr.length-1;
+
+  return arr;
+}
+console.log( 'removeValueFromTheFirstPosOfTheArray', removeValueFromTheFirstPosOfTheArray( [1, 2, 3, 4, 5] ) );
